ef5200193a8931cc3cfc06e375461fc4517aaa50
Ivan Adithya <ivanjayasooriya03@gmail.com>
2025-11-09 13:50:52 +0530

    add login-page

---
+++ script.js
@@ -0,0 +1,11 @@
+// login
+const USER = {username: "ivan", password: "123"};
+
+$("#login-form").on("submit", e => {
+    e.preventDefault();
+    if ($("#username").val() === USER.username && $("#password").val() === USER.password) {
+        $("#login-page").hide();
+        $("#main-app").show();
+
+    } else {$("#login-error").fadeIn().delay(1500).fadeOut();}
+});
